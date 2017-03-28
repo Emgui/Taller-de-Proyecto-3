31ac79df7bcad30d36b7bbdd49e12cf1d405f322
Emgui <u201416289@upc.edu.pe>
2017-03-27 21:11:37 -0500

    Add files via upload

---
+++ webAppPetCenter/GestionPedigri.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Template/Main.Master" AutoEventWireup="true" CodeBehind="GestionPedigri.aspx.cs" Inherits="webAppPetCenter.GestionPedigri" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder" runat="server">
+    <div class="panel-default">
+  <h4>Registro de Solicitud de Pedigri</h4>
+    </div>
+    <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Datos de Cliente</label>
+    <div class="col-sm-10">
+          
+        <button type="button" class="btn btn-info btn-sm">
+            <span class="glyphicon glyphicon-star" aria-hidden="true"></span> 
+  <span class="glyphicon glyphicon-search" aria-hidden="true"></span> Buscar Cliente
+</button>     
+    </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentScriptHolder" runat="server">
+</asp:Content>
---
+++ webAppPetCenter/Template/Main.Master.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PetCenterBL;
+using PetCenterDL.Persistencia;
+using System.Text;
+
+namespace webAppPetCenter.Template
+{
+    public partial class Main : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ListarMenu();
+            }
+        }
+
+        private void ListarMenu()
+        {
+            AplicacionBL aplicacion = AplicacionBL.Instance;
+            var x = aplicacion.ObtenerMenu();
+            StringBuilder cadena = new StringBuilder();
+
+            cadena.Append("<li class='dropdown'>");
+            cadena.Append("<a class='dropdown-toggle' data-toggle='dropdown' href='#'>Mantenimiento</a>");
+            cadena.Append("<ul class='dropdown-menu'>");
+            foreach (var item in x)
+            {                
+                cadena.AppendFormat("<li><a href='{1}'  >{0}</a></li>", item.Nombre, item.Controlador);                 
+            }
+            cadena.Append("</ul>");
+
+            MenuPrincipal.Text = cadena.ToString();
+        }
+    }
+}
